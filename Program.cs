17464fbc9914158191a7b0c783aa40154955e237
Andy Collins <askingalot@gmail.com>
2019-02-26 21:28:11 -0600

    inital commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace befunge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0 ) {
+                Console.WriteLine("Usage: befunge <source.bf>");
+                Environment.Exit(1);
+            }
+            Console.WriteLine($"run {args[0]}");
+        }
+    }
+}
